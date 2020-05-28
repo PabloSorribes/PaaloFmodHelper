13695ad83c1eac2ad7b9f2b467c3bc68c2ddc53e
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2020-05-28 07:27:11 +0200

    Added an extension class for the StudioEventEmitter

---
+++ Assets/Game/Scripts/Extensions/StudioEventEmitterExtensions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using FMODUnity;
+
+namespace Paalo.Fmod
+{
+	public static class StudioEventEmitterExtensions
+	{
+		/// <summary>
+		/// Play this emitter if it isn't already playing (avoid multiple instances triggering in eg. Update()).
+		/// </summary>
+		/// <param name="eventEmitter"></param>
+		public static void PlayIfNotPlaying(this StudioEventEmitter eventEmitter)
+		{
+			if (!eventEmitter.IsPlaying())
+				eventEmitter.Play();
+		}
+	}
+}
