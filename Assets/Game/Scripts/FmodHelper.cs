d6f4eef62531f2c864fd1526afd74552e75251ea
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2020-05-28 07:21:55 +0200

    made whole FmodHelper-class static

--- Assets/Game/Scripts/FmodHelper.cs
+++ Assets/Game/Scripts/FmodHelper.cs
@@ -13,13 +13,12 @@ namespace Paalo.Fmod
 	/// <para></para>
 	/// - Playing an emitter only if isn't already playing with: <see cref="PlayEmitterIfNotPlaying(FMODUnity.StudioEventEmitter)"/>
 	/// </summary>
-	public class FmodHelper : GenericSingleton<FmodHelper>
+	public static class FmodHelper
 	{
 		#region BUSSES
-
 		public enum AudioBusses { MainBus, MusicBus, SfxBus }
 
-		public FMOD.Studio.Bus[] AllBusses => new FMOD.Studio.Bus[] { MasterBus, MusicBus, SfxBus };
+		public static FMOD.Studio.Bus[] AllBusses => new FMOD.Studio.Bus[] { MasterBus, MusicBus, SfxBus };
 
 		//Change the strings here to the paths that your buses have.
 		private static FMOD.Studio.Bus? masterBus;
@@ -55,15 +54,13 @@ namespace Paalo.Fmod
 			}
 		}
 
-
 		private static float busVolumeToSet = 1;
-
 		#endregion BUSSES
 
 		/// <summary>
 		/// Makes Audio run in background (when Alt-Tabbing for live-mixing and so on).
 		/// </summary>
-		private void SetAudioPlaysInBackground(bool playInBackground)
+		public static void SetAudioPlaysInBackground(bool playInBackground)
 		{
 			Application.runInBackground = playInBackground;
 		}
@@ -71,7 +68,7 @@ namespace Paalo.Fmod
 		/// <summary>
 		/// <paramref name="mute"/> → TRUE for turning off audio. FALSE for turning it back on.
 		/// </summary>
-		public void MuteBus(AudioBusses busToMute, bool mute)
+		public static void MuteBus(AudioBusses busToMute, bool mute)
 		{
 			switch (busToMute)
 			{
@@ -97,7 +94,7 @@ namespace Paalo.Fmod
 		/// <para></para> "<paramref name="volumeToChangeWith"/>" = 0 → No volume, -80 dB.
 		/// <para></para> "<paramref name="volumeToChangeWith"/>" = 1 → Max volume, the level of the Bus's fader set in the Fmod-project.
 		/// </summary>
-		public void SetBusVolume(AudioBusses busToChangeVolumeOn, float volumeToChangeWith)
+		public static void SetBusVolume(AudioBusses busToChangeVolumeOn, float volumeToChangeWith)
 		{
 			busVolumeToSet += volumeToChangeWith;
 			busVolumeToSet = Mathf.Clamp01(busVolumeToSet);
@@ -122,7 +119,7 @@ namespace Paalo.Fmod
 			//print(busVolumeToSet);
 		}
 
-		public float GetBusVolume(AudioBusses bus)
+		public static float GetBusVolume(AudioBusses bus)
 		{
 			float volume = 0;
 			float final;
@@ -150,7 +147,7 @@ namespace Paalo.Fmod
 		/// <summary>
 		/// Returns an instance to use in OneShot-functions.
 		/// </summary>
-		public FMOD.Studio.EventInstance CreateFmodEventInstance(string eventName)
+		public static FMOD.Studio.EventInstance CreateFmodEventInstance(string eventName)
 		{
 			return FMODUnity.RuntimeManager.CreateInstance(eventName);
 		}
@@ -158,7 +155,7 @@ namespace Paalo.Fmod
 		/// <summary>
 		/// Use for 2D-events. <paramref name="eventPath"/> is a string to the fmod event, eg. "event:/Arena/countDown".
 		/// </summary>
-		public void PlayOneShot(string eventPath)
+		public static void PlayOneShot(string eventPath)
 		{
 			FMODUnity.RuntimeManager.PlayOneShot(eventPath);
 		}
@@ -166,7 +163,7 @@ namespace Paalo.Fmod
 		/// <summary>
 		/// For setting a parameter before playing.
 		/// </summary>
-		public void PlayOneShot(string eventPath, string parameterName, float parameterValue)
+		public static void PlayOneShot(string eventPath, string parameterName, float parameterValue)
 		{
 			var eventInstance = CreateFmodEventInstance(eventPath);
 
@@ -175,7 +172,7 @@ namespace Paalo.Fmod
 			eventInstance.release();
 		}
 
-		public void PlayOneShotAttached(string eventPath, GameObject gameObject, string parameterName = "", float parameterValue = 0f)
+		public static void PlayOneShotAttached(string eventPath, GameObject gameObject, string parameterName = "", float parameterValue = 0f)
 		{
 			var transform = gameObject.GetComponent<Transform>();
 			var rb = gameObject.GetComponent<Rigidbody>();
@@ -192,7 +189,7 @@ namespace Paalo.Fmod
 		/// <summary>
 		/// For playing 3D-events at a set position, without setting a parameter. "<paramref name="position"/>" could be your transform.position.
 		/// </summary>
-		public void PlayOneShot3D(string eventPath, Vector3 position)
+		public static void PlayOneShot3D(string eventPath, Vector3 position)
 		{
 			FMODUnity.RuntimeManager.PlayOneShot(eventPath, position);
 		}
@@ -200,11 +197,11 @@ namespace Paalo.Fmod
 		/// <summary>
 		/// Use this for overriding the event's 3D-attenuation settings (the min/max distance of the sound, usually set in the 3D-panner of the event).
 		/// </summary>
-		public void PlayOneShot3D(string eventPath, Vector3 position, float minDistanceOverride, float maxDistanceOverride)
+		public static void PlayOneShot3D(string eventPath, Vector3 position, float minDistanceOverride, float maxDistanceOverride)
 		{
-			FMOD.Studio.EventInstance eventInstance = FmodHelper.Instance.CreateFmodEventInstance(eventPath);
+			FMOD.Studio.EventInstance eventInstance = CreateFmodEventInstance(eventPath);
 
-			FmodHelper.Instance.Set3DAttenuationSettings(eventInstance, minDistanceOverride, maxDistanceOverride);
+			Set3DAttenuationSettings(eventInstance, minDistanceOverride, maxDistanceOverride);
 
 			eventInstance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(position));
 			eventInstance.start();
@@ -214,7 +211,7 @@ namespace Paalo.Fmod
 		/// <summary>
 		/// For playing 3D-events at a set position and setting a parameter before playing a 3D-event.
 		/// </summary>
-		public void PlayOneShot3D(string eventPath, Vector3 position, string parameterName, float parameterValue)
+		public static void PlayOneShot3D(string eventPath, Vector3 position, string parameterName, float parameterValue)
 		{
 			var eventInstance = CreateFmodEventInstance(eventPath);
 
@@ -227,11 +224,11 @@ namespace Paalo.Fmod
 		/// <summary>
 		/// For playing 3D-events at a set position and setting a parameter before playing a 3D-event.
 		/// </summary>
-		public void PlayOneShot3D(string eventPath, Vector3 position, string parameterName, float parameterValue, float minDistanceOverride, float maxDistanceOverride)
+		public static void PlayOneShot3D(string eventPath, Vector3 position, string parameterName, float parameterValue, float minDistanceOverride, float maxDistanceOverride)
 		{
 			var eventInstance = CreateFmodEventInstance(eventPath);
 
-			FmodHelper.Instance.Set3DAttenuationSettings(eventInstance, minDistanceOverride, maxDistanceOverride);
+			Set3DAttenuationSettings(eventInstance, minDistanceOverride, maxDistanceOverride);
 
 			eventInstance.setParameterByName(parameterName, parameterValue);
 			eventInstance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(position));
@@ -242,11 +239,18 @@ namespace Paalo.Fmod
 		/// <summary>
 		/// Use to add a StudioEventEmitter on an object. Set your StudioEventEmitter-variable to this function.
 		/// </summary>
-		public FMODUnity.StudioEventEmitter InitializeAudioOnObject(GameObject gameObject, string eventPath)
+		public static FMODUnity.StudioEventEmitter InitializeAudioOnObject(GameObject gameObject, string eventPath)
 		{
-			var fmodEventEmitter = gameObject.AddComponent<FMODUnity.StudioEventEmitter>();
+			var fmodEventEmitter = gameObject.GetComponent<FMODUnity.StudioEventEmitter>();
+
+			//Add a StudioEventEmitter component if there is none on the object.
+			if (fmodEventEmitter == null)
+			{
+				fmodEventEmitter = gameObject.AddComponent<FMODUnity.StudioEventEmitter>();
+			}
 			fmodEventEmitter.Event = eventPath;
 			return fmodEventEmitter;
+
 			//TODO: Add the created event to an array or something.
 		}
 
@@ -254,18 +258,17 @@ namespace Paalo.Fmod
 		/// Use to add a StudioEventEmitter on an object and overriding its Distance Attenuation Settings. 
 		/// Set your StudioEventEmitter-variable to this function.
 		/// </summary>
-		public FMODUnity.StudioEventEmitter InitializeAudioOnObject(GameObject gameObject, string eventPath, float minDistanceOverride, float maxDistanceOverride)
+		public static FMODUnity.StudioEventEmitter InitializeAudioOnObject(GameObject gameObject, string eventPath, float minDistanceOverride, float maxDistanceOverride)
 		{
-			var fmodEventEmitter = gameObject.AddComponent<FMODUnity.StudioEventEmitter>();
-			fmodEventEmitter.Event = eventPath;
-			FmodHelper.Instance.Set3DAttenuationSettings(fmodEventEmitter, minDistanceOverride, maxDistanceOverride);
+			var fmodEventEmitter = InitializeAudioOnObject(gameObject, eventPath);
+			Set3DAttenuationSettings(fmodEventEmitter, minDistanceOverride, maxDistanceOverride);
 			return fmodEventEmitter;
 		}
 
 		/// <summary>
 		/// Plays the emitter if it isn't playing already.
 		/// </summary>
-		public void PlayEmitterIfNotPlaying(FMODUnity.StudioEventEmitter fmodComponent)
+		public static void PlayEmitterIfNotPlaying(FMODUnity.StudioEventEmitter fmodComponent)
 		{
 			if (!fmodComponent.IsPlaying())
 				fmodComponent.Play();
@@ -278,7 +281,7 @@ namespace Paalo.Fmod
 		/// TRUE = Play sound.
 		/// <para></para> FALSE = Stop the sound.
 		/// </summary>
-		public void PlayOrStopSound(FMODUnity.StudioEventEmitter fmodComponent, bool play)
+		public static void PlayOrStopSound(FMODUnity.StudioEventEmitter fmodComponent, bool play)
 		{
 			if (play)
 				fmodComponent.Play();
@@ -291,22 +294,22 @@ namespace Paalo.Fmod
 		/// <para></para>
 		/// This will only work for events that are currently playing, ie. you cannot set this before playing a OneShot. Use <see cref="FMODUnity.RuntimeManager.CreateInstance(string)"/> for that instead.
 		/// </summary>
-		public void SetParameter(FMODUnity.StudioEventEmitter eventEmitter, string parameterName, float parameterValue)
+		public static void SetParameter(FMODUnity.StudioEventEmitter eventEmitter, string parameterName, float parameterValue)
 		{
 			eventEmitter.SetParameter(parameterName, parameterValue);
 		}
 
-		public void SetParameter(FMOD.Studio.EventInstance eventInstance, string parameterName, float parameterValue)
+		public static void SetParameter(FMOD.Studio.EventInstance eventInstance, string parameterName, float parameterValue)
 		{
 			eventInstance.setParameterByName(parameterName, parameterValue);
 		}
 
-		public float GetParameterValue(FMODUnity.StudioEventEmitter eventEmitter, string parameterName)
+		public static float GetParameterValue(FMODUnity.StudioEventEmitter eventEmitter, string parameterName)
 		{
-			return FmodHelper.Instance.GetParameterValue(eventEmitter.EventInstance, parameterName);
+			return GetParameterValue(eventEmitter.EventInstance, parameterName);
 		}
 
-		public float GetParameterValue(FMOD.Studio.EventInstance eventInstance, string parameterName)
+		public static float GetParameterValue(FMOD.Studio.EventInstance eventInstance, string parameterName)
 		{
 			eventInstance.getParameterByName(parameterName, out float paramValue);
 			return paramValue;
@@ -315,7 +318,7 @@ namespace Paalo.Fmod
 		/// <summary>
 		/// <see cref="Vector2.x"/> = Minimum, <see cref="Vector2.y"/> = Maximum. 
 		/// </summary>
-		public Vector2 GetParameterRange(FMODUnity.StudioEventEmitter eventEmitter, string parameterName)
+		public static Vector2 GetParameterRange(FMODUnity.StudioEventEmitter eventEmitter, string parameterName)
 		{
 			eventEmitter.EventDescription.getParameterDescriptionByName(parameterName, out FMOD.Studio.PARAMETER_DESCRIPTION paramDescription);
 
@@ -325,7 +328,7 @@ namespace Paalo.Fmod
 			return parameterRange;
 		}
 
-		public bool IsEventInstancePlaying(FMOD.Studio.EventInstance eventInstance)
+		public static bool IsEventInstancePlaying(FMOD.Studio.EventInstance eventInstance)
 		{
 			eventInstance.getPlaybackState(out FMOD.Studio.PLAYBACK_STATE playbackState);
 			bool isPlaying = playbackState != FMOD.Studio.PLAYBACK_STATE.STOPPED;
@@ -336,7 +339,7 @@ namespace Paalo.Fmod
 		/// Doesn't seem to work until the 3rd or 4th frame for some reason.
 		/// </summary>
 		/// <param name="eventEmitter"></param>
-		public void SetRandomTimelinePosition(FMODUnity.StudioEventEmitter eventEmitter)
+		public static void SetRandomTimelinePosition(FMODUnity.StudioEventEmitter eventEmitter)
 		{
 			SetRandomTimelinePosition(eventEmitter.EventInstance);
 		}
@@ -345,7 +348,7 @@ namespace Paalo.Fmod
 		/// Doesn't seem to work until the 3rd or 4th frame for some reason.
 		/// </summary>
 		/// <param name="eventEmitter"></param>
-		public void SetRandomTimelinePosition(FMOD.Studio.EventInstance eventInstance)
+		public static void SetRandomTimelinePosition(FMOD.Studio.EventInstance eventInstance)
 		{
 			eventInstance.getDescription(out FMOD.Studio.EventDescription eventDescription);
 			eventDescription.getLength(out int eventLength);
@@ -361,7 +364,7 @@ namespace Paalo.Fmod
 		/// <param name="eventEmitter"></param>
 		/// <param name="minDistance"></param>
 		/// <param name="maxDistance"></param>
-		public void Set3DAttenuationSettings(FMODUnity.StudioEventEmitter eventEmitter, float minDistance, float maxDistance)
+		public static void Set3DAttenuationSettings(FMODUnity.StudioEventEmitter eventEmitter, float minDistance, float maxDistance)
 		{
 			//Stop minDistance from being greater than maxDistance and vice versa.
 			minDistance = Mathf.Clamp(minDistance, 0, maxDistance);
@@ -378,7 +381,7 @@ namespace Paalo.Fmod
 		/// <param name="eventInstance"></param>
 		/// <param name="minDistance"></param>
 		/// <param name="maxDistance"></param>
-		public void Set3DAttenuationSettings(FMOD.Studio.EventInstance eventInstance, float minDistance, float maxDistance)
+		public static void Set3DAttenuationSettings(FMOD.Studio.EventInstance eventInstance, float minDistance, float maxDistance)
 		{
 			//Stop minDistance from being greater than maxDistance and vice versa.
 			minDistance = Mathf.Clamp(minDistance, 0, maxDistance);
@@ -395,7 +398,7 @@ namespace Paalo.Fmod
 		/// <param name="eventEmitter"></param>
 		/// <param name="minDistance"></param>
 		/// <param name="maxDistance"></param>
-		public void Get3DAttenuationSettings(FMODUnity.StudioEventEmitter eventEmitter, out float minDistance, out float maxDistance)
+		public static void Get3DAttenuationSettings(FMODUnity.StudioEventEmitter eventEmitter, out float minDistance, out float maxDistance)
 		{
 			Get3DAttenuationSettings(eventEmitter.EventInstance, out minDistance, out maxDistance);
 		}
@@ -406,7 +409,7 @@ namespace Paalo.Fmod
 		/// <param name="eventInstance"></param>
 		/// <param name="minDistance"></param>
 		/// <param name="maxDistance"></param>
-		public void Get3DAttenuationSettings(FMOD.Studio.EventInstance eventInstance, out float minDistance, out float maxDistance)
+		public static void Get3DAttenuationSettings(FMOD.Studio.EventInstance eventInstance, out float minDistance, out float maxDistance)
 		{
 			eventInstance.getProperty(FMOD.Studio.EVENT_PROPERTY.MINIMUM_DISTANCE, out minDistance);
 			eventInstance.getProperty(FMOD.Studio.EVENT_PROPERTY.MAXIMUM_DISTANCE, out maxDistance);
